5ec58077b01f0339ba5a3244376b180ea6ea9635
Zubin Duggal <zubin@cmi.ac.in>
2019-07-18 22:35:19 +0530

    POC cabal-helper-helper

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for cabal-helper-helper
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ Main.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE TupleSections #-}
+module Main where
+  
+import Distribution.Helper
+import Distribution.Helper.Discover
+
+import System.Environment
+import System.FilePath
+
+import qualified Data.Map as M
+import Data.Monoid
+import Data.Foldable (toList)
+import Data.List (tails, intersperse, sortOn)
+import Data.Maybe (listToMaybe)
+import Data.Ord
+import Debug.Trace
+
+main :: IO ()
+main = do
+  [root,file] <- getArgs
+  let file_dir = takeDirectory file
+  projs <- findProjects root
+  case projs of
+    (Ex proj:_) -> do
+      let [dist_dir] = findDistDirs proj
+      env <- mkQueryEnv proj dist_dir
+      units <- runQuery (allUnits id) env
+      case (getFlags file_dir $ toList units) of
+        Just fs -> mapM_ putStr (intersperse " " $ fs++[file]) >> putStrLn ""
+
+getFlags :: FilePath -> [UnitInfo] -> Maybe [String]
+getFlags dir uis
+  = listToMaybe
+  $ map (ciGhcOptions . snd)
+  $ filter (hasParent dir . fst)
+  $ sortOn (Down . length . fst)
+  $ concatMap (\ci -> map (,ci) (ciSourceDirs ci))
+  $ concat
+  $ M.elems . uiComponents <$> uis
+
+hasParent :: FilePath -> FilePath -> Bool
+hasParent child parent = any (equalFilePath parent) (map joinPath $ tails $ splitPath child)
