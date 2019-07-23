3f3a8be36af8979c2c36be28d52789532d62b820
Zubin Duggal <zubin@cmi.ac.in>
2019-07-23 15:31:03 +0530

    fix component guessing

--- Main.hs
+++ Main.hs
@@ -6,30 +6,36 @@ import Distribution.Helper.Discover
 
 import System.Environment
 import System.FilePath
+import System.Directory
+import System.IO
 
 import qualified Data.Map as M
 import Data.Monoid
 import Data.Foldable (toList)
-import Data.List (tails, intersperse, sortOn)
+import Data.List (inits, intersperse, sortOn)
 import Data.Maybe (listToMaybe)
 import Data.Ord
 import Debug.Trace
 
 main :: IO ()
 main = do
-  [root,file] <- getArgs
-  let file_dir = takeDirectory file
+  [root',file'] <- getArgs
+  root <- canonicalizePath root'
+  file <- canonicalizePath file'
+  let file_dir = makeRelative root $ takeDirectory file
+  mapM_ (hPutStrLn stderr) [root, file_dir]
   projs <- findProjects root
   case projs of
     (Ex proj:_) -> do
       let [dist_dir] = findDistDirs proj
       env <- mkQueryEnv proj dist_dir
       units <- runQuery (allUnits id) env
+      -- mapM_ (hPutStrLn stderr . show) units
       case (getFlags file_dir $ toList units) of
         Just fs -> mapM_ putStr (showForSh $ fs++[file]) >> putStrLn ""
 
 showForSh :: [String] -> [String]
-showForSh = intersperse " " . map (\x -> "'"++x++"'")
+showForSh = intersperse " " -- . map (\x -> "'"++x++"'")
 
 getFlags :: FilePath -> [UnitInfo] -> Maybe [String]
 getFlags dir uis
@@ -42,4 +48,4 @@ getFlags dir uis
   $ M.elems . uiComponents <$> uis
 
 hasParent :: FilePath -> FilePath -> Bool
-hasParent child parent = any (equalFilePath parent) (map joinPath $ tails $ splitPath child)
+hasParent child parent = any (equalFilePath parent) (map joinPath $ inits $ splitPath child)
